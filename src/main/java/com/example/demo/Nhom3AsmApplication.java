19af5a8ed107ad3501a38802b38c5a9804cf8c96
beDat <tandat10c9@gmail.com>
2024-11-07 13:45:17 +0700

    create project ASM CLA

---
+++ src/main/java/com/example/demo/Nhom3AsmApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nhom3AsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nhom3AsmApplication.class, args);
+	}
+
+}
